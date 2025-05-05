0bbe5c69ff20fee31e5c0981ca840474bc071213
LuckMeelo <charmeel.vodouhe@epitech.eu>
2025-05-05 10:53:39 +0200

    chore: remove prettier end of line lint errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -27,6 +27,12 @@ export default tseslint.config(
   },
   {
     rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          endOfLine: 'auto',
+        },
+      ],
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
