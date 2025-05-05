2c49e18b03757a318c9d2cfe4b6bf98a66a9f33d
Melimeelo <charmeel.vodouhe@epitech.eu>
2025-05-05 16:35:34 +0100

    fix: remove unused route POST /api/users (#37)

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,17 +1,6 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  HttpCode,
-  HttpStatus,
-  Request,
-  UseGuards,
-} from '@nestjs/common';
-import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Request, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { User } from './schemas/user.schema';
 import { JwtAuthGuard } from 'src/modules/auth/guards/jwt-auth.guard';
 
 @ApiTags('users')
@@ -19,61 +8,10 @@ import { JwtAuthGuard } from 'src/modules/auth/guards/jwt-auth.guard';
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Post()
-  @ApiOperation({ summary: 'Create a new user' })
-  @ApiResponse({
-    status: 201,
-    description: 'User successfully created',
-    type: User,
-  })
-  @HttpCode(HttpStatus.CREATED)
-  create(@Body() createUserDto: CreateUserDto): Promise<User> {
-    return this.usersService.create(createUserDto);
-  }
-
   @Get('me')
   @UseGuards(JwtAuthGuard)
   @ApiBearerAuth('access-token')
   async getProfile(@Request() req) {
     return await this.usersService.findByEmail(req.user.email as string);
   }
-
-  // @Get()
-  // @ApiOperation({ summary: 'Get all users' })
-  // @ApiResponse({ status: 200, description: 'Return all users', type: [User] })
-  // findAll(): Promise<User[]> {
-  //   return this.usersService.findAll();
-  // }
-
-  // @Get(':id')
-  // @ApiOperation({ summary: 'Get a user by id' })
-  // @ApiResponse({ status: 200, description: 'Return the user', type: User })
-  // @ApiResponse({ status: 404, description: 'User not found' })
-  // findOne(@Param('id') id: number): Promise<User> {
-  //   return this.usersService.findOne(id);
-  // }
-
-  // @Patch(':id')
-  // @ApiOperation({ summary: 'Update a user' })
-  // @ApiResponse({
-  //   status: 200,
-  //   description: 'User successfully updated',
-  //   type: User,
-  // })
-  // @ApiResponse({ status: 404, description: 'User not found' })
-  // update(
-  //   @Param('id') id: number,
-  //   @Body() updateUserDto: UpdateUserDto,
-  // ): Promise<User> {
-  //   return this.usersService.update(id, updateUserDto);
-  // }
-
-  // @Delete(':id')
-  // @ApiOperation({ summary: 'Delete a user' })
-  // @ApiResponse({ status: 204, description: 'User successfully deleted' })
-  // @ApiResponse({ status: 404, description: 'User not found' })
-  // @HttpCode(HttpStatus.NO_CONTENT)
-  // remove(@Param('id') id: number): Promise<void> {
-  //   return this.usersService.remove(id);
-  // }
 }
